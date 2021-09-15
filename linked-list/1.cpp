9be851a2de529f698d299b8a44b8603f4f5b070a
shikharsun <shikhar21121999@gmail.com>
2021-09-15 07:59:27 +0530

    reverse iterative working, reverse recursive debugging left

--- linked-list/1.cpp
+++ linked-list/1.cpp
@@ -3,51 +3,95 @@ using namespace std;
 
 class Node{
 public:
-	int a;
+	int data;
 	Node* next;
 
 	Node() {
-		data = 0;
-		next = NULL;
+		this->data = 0;
+		this->next = NULL;
 	}
-}
+
+	Node (int a) {
+		this->data = a;
+		this->next = NULL;
+	}
+};
 
 class LinkedList {
 public:
 	Node* head;
 	LinkedList() {
-		head = NULL
+		this->head = NULL;
 	}
 
 	void reverseIterative () {
-		Node* curr = head;
-		Node* prev = NULL
+		Node* curr = this->head;
+		Node* prev = NULL;
+		Node* next = NULL;
 		while(curr != NULL) {
-			Node* next = curr->next
-			curr->next = prev
-			prev = curr
-			curr = next
+			next = curr->next;
+			curr->next = prev;
+			prev = curr;
+			curr = next;
 		}
-		head = prev
+		this->head = prev;
 	}
 
-	Node* reverseRecur (Node* head) {
-		if (head == NULL || head->next == NULL) {
-			return head;
+	void insertOne (int d) {
+
+		Node* newNode = new Node(d);
+		if (this->head == NULL) {
+			this->head = newNode;
+			return;
+		}
+
+		Node* curr = this->head;
+		while(curr->next != NULL) {
+			curr = curr->next;
+		}
+
+
+		curr->next = newNode;
+	}
+
+	void printList () {
+		
+		Node* curr = this->head;
+
+		if(this->head == NULL) {
+			cout<<"List Empty"<<endl;
+			return;
+		}
+		while(curr != NULL){
+			cout << curr->data << " ";
+			curr = curr->next;
+		}
+	}
+
+	void reverseRecur () {
+		if (this->head == NULL || this->head->next == NULL) {
+			return;
 		} else {
-			Node* rest = reverseRecur(head->next);
-			head->next->next = head;
-			
-			head->next = NULL;
-
-			return rest;
+			// break it into two linked list
+			LinkedList rest;
+			rest->head = this->head->next;
+			rest.reverseRecur();
+			// Node* rest = reverseRecur(head->next);
+			this->head->next->next = head;
+			this->head->next = NULL;
+			return;
 		}
 	}
-}
+};
 
 int main() {
-	int a;
-	cin>>a;
-	cout<<"Test"<<endl;
-	cout<<a<<endl;
+	// create a linked list
+	LinkedList l1;
+	l1.insertOne(4);
+	l1.insertOne(5);
+	l1.insertOne(7);
+	l1.printList();
+	cout<<endl;
+	l1.reverseRecur();
+	l1.printList();
 }
